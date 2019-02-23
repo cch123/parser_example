b6596f31ecf9a19600702a3ada7a93cb76b63965
Xargin <cao1988228@163.com>
2019-02-23 19:15:15 +0800

    add ast conversion

--- go-peg/bool_parser/parser.go
+++ go-peg/bool_parser/parser.go
@@ -3,9 +3,26 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strings"
 
 	gopeg "github.com/yhirose/go-peg"
 )
+
+type andExpr struct {
+	lhs interface{} // comp, expr
+	rhs interface{} // comp, expr
+}
+
+type orExpr struct {
+	lhs interface{} // comp, expr
+	rhs interface{} // comp, expr
+}
+
+type comp struct {
+	lhs string
+	rhs string
+	op  string
+}
 
 func main() {
 	contentBytes, err := ioutil.ReadFile("./grammar.peg")
@@ -27,36 +44,53 @@ func main() {
 		return
 	}
 
-	ast, err := parser.ParseAndGetAst("(a=1)", nil)
+	ast, err := parser.ParseAndGetAst("(a=1 or b = 2 and c =4)", nil)
 	if err != nil {
 		fmt.Println("ff", err)
 		return
 	}
-	traverseAST(ast)
+	myAST := traverseAST(ast)
+	fmt.Printf("%#v\n", myAST)
 }
 
-func traverseAST(ast *gopeg.Ast) {
-	println(ast.Name)
+func traverseAST(ast *gopeg.Ast) interface{} {
 	switch ast.Name {
 	case "EXPR":
 		// fold
-		println(ast.Nodes[0].Name)
-		traverseAST(ast.Nodes[0])
+		initial := traverseAST(ast.Nodes[0])
+		var lhs interface{} = initial
+		idx := 1
+		for idx < len(ast.Nodes)-1 {
+			binop := ast.Nodes[idx]
+			rhs := traverseAST(ast.Nodes[idx+1])
+			fmt.Printf("%#v\n", binop.S)
+			if strings.TrimSpace(binop.S) == "and" {
+				lhs = andExpr{lhs: lhs, rhs: rhs}
+			} else if strings.TrimSpace(binop.S) == "or" {
+				lhs = orExpr{lhs: lhs, rhs: rhs}
+			} else {
+				fmt.Printf("failed %#v\n", binop.S)
+				break
+			}
+			idx += 2
+		}
+		return lhs
 	case "ATOM":
 		// go deeper
 		// (FIELD OP VALUE) / '(' EXPR ')'
-		// return
-		println(ast.Nodes[0].Name)
 		inner := ast.Nodes[0]
 		switch inner.Name {
 		case "EXPR":
-			traverseAST(inner)
+			return traverseAST(inner)
 		case "COMP":
-			println(inner.Nodes[0].Name)
-			println(inner.Nodes[1].Name)
-			println(inner.Nodes[2].Name)
+			return comp{
+				lhs: inner.Nodes[0].Token,
+				op:  strings.TrimSpace(inner.Nodes[1].S), // 这里 token 取出来是空的，应该是 bug
+				rhs: strings.TrimSpace(inner.Nodes[2].S), // 这里 token 取出来是空的
+			}
 		}
 	default:
 		println("fuck")
 	}
+	return ""
 }
