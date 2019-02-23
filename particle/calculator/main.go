cda971f193afaa6fff52a7dfc3e22b21a50de15b
Xargin <cao1988228@163.com>
2019-02-23 23:55:19 +0800

    add particle demo

---
+++ particle/calculator/main.go
@@ -0,0 +1,206 @@
+// nolint: govet
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math"
+	"os"
+	"strings"
+
+	"github.com/alecthomas/kong"
+	"github.com/alecthomas/participle"
+)
+
+var cli struct {
+	AST        bool               `help:"Print AST for expression."`
+	Set        map[string]float64 `short:"s" help:"Set variables."`
+	Expression []string           `arg required help:"Expression to evaluate."`
+}
+
+type Operator int
+
+const (
+	OpMul Operator = iota
+	OpDiv
+	OpAdd
+	OpSub
+)
+
+var operatorMap = map[string]Operator{"+": OpAdd, "-": OpSub, "*": OpMul, "/": OpDiv}
+
+func (o *Operator) Capture(s []string) error {
+	*o = operatorMap[s[0]]
+	return nil
+}
+
+// E --> T {( "+" | "-" ) T}
+// T --> F {( "*" | "/" ) F}
+// F --> P ["^" F]
+// P --> v | "(" E ")" | "-" T
+
+type Value struct {
+	Number        *float64    `  @(Float|Int)`
+	Variable      *string     `| @Ident`
+	Subexpression *Expression `| "(" @@ ")"`
+}
+
+type Factor struct {
+	Base     *Value `@@`
+	Exponent *Value `[ "^" @@ ]`
+}
+
+type OpFactor struct {
+	Operator Operator `@("*" | "/")`
+	Factor   *Factor  `@@`
+}
+
+type Term struct {
+	Left  *Factor     `@@`
+	Right []*OpFactor `{ @@ }`
+}
+
+type OpTerm struct {
+	Operator Operator `@("+" | "-")`
+	Term     *Term    `@@`
+}
+
+type Expression struct {
+	Left  *Term     `@@`
+	Right []*OpTerm `{ @@ }`
+}
+
+// Display
+
+func (o Operator) String() string {
+	switch o {
+	case OpMul:
+		return "*"
+	case OpDiv:
+		return "/"
+	case OpSub:
+		return "-"
+	case OpAdd:
+		return "+"
+	}
+	panic("unsupported operator")
+}
+
+func (v *Value) String() string {
+	if v.Number != nil {
+		return fmt.Sprintf("%g", *v.Number)
+	}
+	if v.Variable != nil {
+		return *v.Variable
+	}
+	return "(" + v.Subexpression.String() + ")"
+}
+
+func (f *Factor) String() string {
+	out := f.Base.String()
+	if f.Exponent != nil {
+		out += " ^ " + f.Exponent.String()
+	}
+	return out
+}
+
+func (o *OpFactor) String() string {
+	return fmt.Sprintf("%s %s", o.Operator, o.Factor)
+}
+
+func (t *Term) String() string {
+	out := []string{t.Left.String()}
+	for _, r := range t.Right {
+		out = append(out, r.String())
+	}
+	return strings.Join(out, " ")
+}
+
+func (o *OpTerm) String() string {
+	return fmt.Sprintf("%s %s", o.Operator, o.Term)
+}
+
+func (e *Expression) String() string {
+	out := []string{e.Left.String()}
+	for _, r := range e.Right {
+		out = append(out, r.String())
+	}
+	return strings.Join(out, " ")
+}
+
+// Evaluation
+
+func (o Operator) Eval(l, r float64) float64 {
+	switch o {
+	case OpMul:
+		return l * r
+	case OpDiv:
+		return l / r
+	case OpAdd:
+		return l + r
+	case OpSub:
+		return l - r
+	}
+	panic("unsupported operator")
+}
+
+func (v *Value) Eval(ctx Context) float64 {
+	switch {
+	case v.Number != nil:
+		return *v.Number
+	case v.Variable != nil:
+		value, ok := ctx[*v.Variable]
+		if !ok {
+			panic("no such variable " + *v.Variable)
+		}
+		return value
+	default:
+		return v.Subexpression.Eval(ctx)
+	}
+}
+
+func (f *Factor) Eval(ctx Context) float64 {
+	b := f.Base.Eval(ctx)
+	if f.Exponent != nil {
+		return math.Pow(b, f.Exponent.Eval(ctx))
+	}
+	return b
+}
+
+func (t *Term) Eval(ctx Context) float64 {
+	n := t.Left.Eval(ctx)
+	for _, r := range t.Right {
+		n = r.Operator.Eval(n, r.Factor.Eval(ctx))
+	}
+	return n
+}
+
+func (e *Expression) Eval(ctx Context) float64 {
+	l := e.Left.Eval(ctx)
+	for _, r := range e.Right {
+		l = r.Operator.Eval(l, r.Term.Eval(ctx))
+	}
+	return l
+}
+
+type Context map[string]float64
+
+func main() {
+	ctx := kong.Parse(&cli,
+		kong.Description("A basic expression parser and evaluator."),
+		kong.UsageOnError(),
+	)
+
+	parser, err := participle.Build(&Expression{})
+	ctx.FatalIfErrorf(err)
+
+	expr := &Expression{}
+	err = parser.ParseString(strings.Join(cli.Expression, " "), expr)
+	ctx.FatalIfErrorf(err)
+
+	if cli.AST {
+		json.NewEncoder(os.Stdout).Encode(expr)
+	} else {
+		fmt.Println(expr, "=", expr.Eval(cli.Set))
+	}
+}
