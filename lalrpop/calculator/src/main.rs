5fae16c3400d2e491be4ccfad6433270c53df22f
Xargin <cao1988228@163.com>
2019-06-25 17:23:59 +0800

    add lalrpop dir

---
+++ lalrpop/calculator/src/main.rs
@@ -0,0 +1,11 @@
+#[macro_use] extern crate lalrpop_util;
+
+lalrpop_mod!(pub calculator1); // synthesized by LALRPOP
+
+#[test]
+fn calculator1() {
+    assert!(calculator1::TermParser::new().parse("22").is_ok());
+    assert!(calculator1::TermParser::new().parse("(22)").is_ok());
+    assert!(calculator1::TermParser::new().parse("((((22))))").is_ok());
+    assert!(calculator1::TermParser::new().parse("((22)").is_err());
+}
