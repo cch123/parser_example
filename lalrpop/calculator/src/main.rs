c0861e5e531b4035e572afd46bb54ff2f65ce454
Xargin <cao1988228@163.com>
2019-06-26 12:58:03 +0800

    update lalr

--- lalrpop/calculator/src/main.rs
+++ lalrpop/calculator/src/main.rs
@@ -1,11 +1,15 @@
 #[macro_use] extern crate lalrpop_util;
 
 lalrpop_mod!(pub calculator1); // synthesized by LALRPOP
+pub mod ast;
+
 
 fn main() {
-    assert!(calculator1::TermParser::new().parse("22").is_ok());
-    assert!(calculator1::TermParser::new().parse("(22)").is_ok());
-    assert!(calculator1::TermParser::new().parse("((((22))))").is_ok());
-    assert!(calculator1::TermParser::new().parse("((22)").is_err());
-    println!("{:?}", calculator1::TermParser::new().parse("((22))"))
+    let expr = calculator1::ExprParser::new()
+        .parse("22 * 44 + 66")
+        .unwrap();
+
+    println!("{:#?}", expr);
+    //assert_eq!(&format!("{:?}", expr), "((22 * 44) + 66)");
 }
+
