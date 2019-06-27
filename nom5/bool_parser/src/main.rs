f9ab3dfe81f8e1c832c5100ff6eccf66c52237f6
Xargin <cao1988228@163.com>
2019-06-27 17:26:27 +0800

    optimize

--- nom5/bool_parser/src/main.rs
+++ nom5/bool_parser/src/main.rs
@@ -5,40 +5,41 @@ use std::fmt::{Debug, Display, Formatter};
 
 use std::str::FromStr;
 
+use nom::bytes::complete::{tag_no_case, take_until, take_while};
+use nom::character::complete::{not_line_ending, space0};
+use nom::character::{is_alphabetic, is_alphanumeric};
+use nom::sequence::{delimitedc, tuple};
 use nom::{
-    IResult,
-    character::complete::{digit1 as digit, multispace0 as multispace, alphanumeric0 as alphanumeric},
+    branch::alt,
     bytes::complete::take_till,
-    sequence::{preceded, delimited},
+    bytes::complete::{is_not, tag},
+    character::complete::{
+        alphanumeric0 as alphanumeric, digit1 as digit, multispace0 as multispace,
+    },
     combinator::{map, map_res},
     multi::many0,
-    branch::alt,
-    bytes::complete::{tag,is_not},
+    sequence::{delimited, preceded},
+    IResult,
 };
-use nom::bytes::complete::{take_until, take_while, tag_no_case};
-use nom::character::{is_alphanumeric, is_alphabetic};
-use nom::character::complete::{space0, not_line_ending};
-use nom::sequence::delimitedc;
-
 
 #[derive(Debug)]
-pub enum BoolExpr <'a>{
+pub enum BoolExpr<'a> {
     CompExpr {
-        field : &'a str,
-        op : &'a str,
-        value : &'a str,
+        field: &'a str,
+        op: &'a str,
+        value: &'a str,
     },
     AndExpr {
-        left : Box<BoolExpr<'a>>,
-        right : Box<BoolExpr<'a>>,
+        left: Box<BoolExpr<'a>>,
+        right: Box<BoolExpr<'a>>,
     },
     OrExpr {
-        left : Box<BoolExpr<'a>>,
-        right : Box<BoolExpr<'a>>,
+        left: Box<BoolExpr<'a>>,
+        right: Box<BoolExpr<'a>>,
     },
     ParenExpr {
-        expr: Box<BoolExpr<'a>>
-    }
+        expr: Box<BoolExpr<'a>>,
+    },
 }
 
 // 求优雅写法
@@ -48,7 +49,13 @@ fn and_expr(i: &str) -> IResult<&str, BoolExpr> {
     let (i, op) = tag_no_case("and")(i)?;
     let (i, _) = space0(i)?;
     let (i, right) = bool_expr(i)?;
-    Ok((i, BoolExpr::AndExpr {left: Box::new(left),right: Box::new(right)}))
+    Ok((
+        i,
+        BoolExpr::AndExpr {
+            left: Box::new(left),
+            right: Box::new(right),
+        },
+    ))
 }
 
 fn or_expr(i: &str) -> IResult<&str, BoolExpr> {
@@ -57,36 +64,42 @@ fn or_expr(i: &str) -> IResult<&str, BoolExpr> {
     let (i, op) = tag_no_case("or")(i)?;
     let (i, _) = space0(i)?;
     let (i, right) = bool_expr(i)?;
-    Ok((i, BoolExpr::OrExpr{left: Box::new(left),right: Box::new(right)}))
+    Ok((
+        i,
+        BoolExpr::OrExpr {
+            left: Box::new(left),
+            right: Box::new(right),
+        },
+    ))
 }
 
 fn paren_expr(i: &str) -> IResult<&str, BoolExpr> {
-    let (i,_) = tag("(")(i)?;
+    let (i, _) = tag("(")(i)?;
     let (i, _) = space0(i)?;
     let (i, expr) = bool_expr(i)?;
     let (i, _) = space0(i)?;
-    let (i,_) = tag(")")(i)?;
+    let (i, _) = tag(")")(i)?;
     Ok((i, expr))
 }
 
 fn bool_expr(i: &str) -> IResult<&str, BoolExpr> {
-    alt((
-        and_expr,
-        or_expr,
-        atom,
-    ))(i)
+    alt((and_expr, or_expr, atom))(i)
 }
 
 fn atom(i: &str) -> IResult<&str, BoolExpr> {
-    alt((
-        paren_expr,
-        comp_expr,
-    ))(i)
+    alt((paren_expr, comp_expr))(i)
 }
 
-
-// 看看能不能优化一下，这么写不简洁，还不如以前的 do_parse 宏简单
 fn comp_expr(i: &str) -> IResult<&str, BoolExpr> {
+    // 优化写法，和下面的是等价的
+    let (i, (left, _, op, _, right)) = tuple((
+        alphanumeric,
+        space0,
+        is_not(" \t\r\nabcdefghijklmnopqrstuvwxyz"),
+        space0,
+        alphanumeric,
+    ))(i)?;
+    /*
     let (i, left) = alphanumeric(i)?;
 
     // 吃掉 space
@@ -100,19 +113,24 @@ fn comp_expr(i: &str) -> IResult<&str, BoolExpr> {
     // 吃掉 space
     let (i, _) = space0(i)?;
     let (i, right) = alphanumeric(i)?;
-    Ok((i, BoolExpr::CompExpr {field: left, op: op, value : right}))
+    */
+    Ok((
+        i,
+        BoolExpr::CompExpr {
+            field: left,
+            op: op,
+            value: right,
+        },
+    ))
 }
-
 
 fn main() {
     //let (s, expr) = comp_expr("a1 = b");
     match bool_expr("(a1 >=b or a = 1) and c > 832") {
-        Ok((s,expr)) => {
+        Ok((s, expr)) => {
             println!("{:#?}", s);
             println!("{:#?}", expr);
-        },
-        Err(e) => {
-            println!("{:#?}", e)
-        },
+        }
+        Err(e) => println!("{:#?}", e),
     }
 }
